63f9839ad5a54a23a7726f0e222800e8965e10a7
Udith S <87334968+xdblanc@users.noreply.github.com>
2023-05-28 01:30:00 +0530

    improved db and edit profile

--- server/ed.js
+++ server/ed.js
@@ -1,42 +1,49 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
 app.use(express.json());
 app.use(cors());
-const mname = 'jaa'; //matcher
 
+// Now you can use the useremail variable in this file
+const sea='jathin@gmail.com'
+const userSchema = new mongoose.Schema({
+  email: String,
+  address: String,
+  fname: String,
+  lname: String,
+  att_c1: String,
+  att_c2: String,
+  att_c3: String,
+  fnam: String,
+  fnum: String,
+  git: String,
+  link: String,
+  mnam: String,
+  mnum: String,
+  sem1: String,
+  sem2: String,
+  sem3: String,
+  sem4: String,
+  sem5: String,
+  image: String,
+  Phone: String,
+});
+
+// Define the User model
+const User = mongoose.model('User', userSchema);
+
+// Connect to the MongoDB database
 mongoose.connect('mongodb+srv://se_tech:se12345@se.wawkg12.mongodb.net/', {
   dbName: 'exp',
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  dob: {
-    type: Date,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-});
-const User = mongoose.model('users', UserSchema);
-
+// Fetch the User document with the provided email
 app.get('/user', async (req, resp) => {
   try {
-    const user = await User.findOne({ name: mname });
+    const user = await User.findOne({ email: sea });
     if (user) {
       resp.send(user);
     } else {
@@ -47,32 +54,32 @@ app.get('/user', async (req, resp) => {
   }
 });
 
+// Update the user data in the database
 app.post('/register', async (req, resp) => {
   try {
-    const user = await User.findOne({ name: mname });
+    const user = await User.findOne({ email: 'jathin@gmail.com' });
     if (user) {
-      user.name = req.body.name || user.name;
-      user.email = req.body.email || user.email;
+      user.fnum = req.body.fnum || user.fnum;
       user.address = req.body.address || user.address;
-      user.dob = req.body.dob || user.dob;
+      user.git = req.body.git || user.git;
+      user.link = req.body.link || user.link;
+      user.mnum = req.body.mnum || user.mnum;
+      user.image = req.body.image || user.image;
+      user.Phone = req.body.Phone || user.Phone;
 
       let result = await user.save();
       result = result.toObject();
-      if (result) {
-        delete result.password;
-        resp.send(result);
-        console.log(result);
-      } else {
-        console.log('User already registered');
-      }
+      delete result.password;
+      resp.send(result);
     } else {
-      console.log('User not found');
+      resp.send('User not found');
     }
   } catch (e) {
     resp.send('Something Went Wrong');
   }
 });
 
+// Start the server
 app.listen(5000, () => {
-  console.log('App listen at port 5000');
+  console.log('App listening at port 5000');
 });
--- server/server.js
+++ server/server.js
@@ -3,112 +3,113 @@
 //price_1Mx5iXSAYLrAqiBD7brI9IRs = Bus Fee ID
 //price_1Mx5jkSAYLrAqiBDiJul5WDO = Mess Fee ID
 
-const express = require('express')
-const mongoose = require('mongoose')
-var cors = require('cors')
-const stripe = require('stripe')('sk_test_51MrzASSAYLrAqiBDunzidLbjPsRp06Uzb6v4kkwDeIG3UzOiKsEKe2Vtud1tPTXZsy4oYXAQzza3uAd7EXROzc5U00XpqowSZ2')
-const mongoUrl = "mongodb+srv://se_tech:se12345@se.wawkg12.mongodb.net/?retryWrites=true&w=majority"
-const app = express()
-const jwt = require('jsonwebtoken')
-const JWT_SECRET = "dsdgassa121231wswewr!@$!@1dasda"
-const bcrypt = require('bcryptjs')
-app.use(cors())
-app.use(express.static("public"))
-app.use(express.json())
+const express = require('express');
+const mongoose = require('mongoose');
+var cors = require('cors');
+const stripe = require('stripe')(
+  'sk_test_51MrzASSAYLrAqiBDunzidLbjPsRp06Uzb6v4kkwDeIG3UzOiKsEKe2Vtud1tPTXZsy4oYXAQzza3uAd7EXROzc5U00XpqowSZ2'
+);
+const mongoUrl =
+  'mongodb+srv://se_tech:se12345@se.wawkg12.mongodb.net/?retryWrites=true&w=majority';
+const app = express();
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'dsdgassa121231wswewr!@$!@1dasda';
+const bcrypt = require('bcryptjs');
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
 
-app.post("/checkout", async (req, res) => {
-
-    console.log(req.body)
-    const items = req.body.items
-    let lineitems = []
-    items.forEach(item => {
-        lineitems.push({
-            price: item.id,
-            quantity: item.quantity
-        })
+app.post('/checkout', async (req, res) => {
+  console.log(req.body);
+  const items = req.body.items;
+  let lineitems = [];
+  items.forEach((item) => {
+    lineitems.push({
+      price: item.id,
+      quantity: item.quantity,
     });
+  });
 
-    const session = await stripe.checkout.sessions.create({
-        line_items: lineitems,
-        mode: 'payment',
-        success_url: 'http://localhost:3001/success',
-        cancel_url: 'http://localhost:3001/cancel'
+  const session = await stripe.checkout.sessions.create({
+    line_items: lineitems,
+    mode: 'payment',
+    success_url: 'http://localhost:3001/success',
+    cancel_url: 'http://localhost:3001/cancel',
+  });
+
+  res.send(
+    JSON.stringify({
+      url: session.url,
     })
+  );
+});
+require('./userDetails');
+const User = mongoose.model('UserInfo');
 
-    res.send(JSON.stringify({
-        url: session.url
-    }))
-
-})
-require('./userDetails')
-const User = mongoose.model('UserInfo')
-
-mongoose.connect(mongoUrl, {
+mongoose
+  .connect(mongoUrl, {
     useNewUrlParser: true,
-})
-    .then(() => {
-        console.log('Success')
-    })
-    .catch((e) => console.log(e))
+  })
+  .then(() => {
+    console.log('Success');
+  })
+  .catch((e) => console.log(e));
 
 app.post('/adminpage', async (req, res) => {
-    const { fname, lname, email, password } = req.body
-    const encrpytedPassword = await bcrypt.hash(password, 10)
-    try {
-        const oldUser = await User.findOne({ email })
-        if (oldUser) {
-            return res.send({ error: 'User Exists' })
-        }
-        await User.create({
-            fname,
-            lname,
-            email,
-            password: encrpytedPassword,
-        })
-        res.send({ status: 'ok' })
+  const { fname, lname, email, password } = req.body;
+  const encrpytedPassword = await bcrypt.hash(password, 10);
+  try {
+    const oldUser = await User.findOne({ email });
+    if (oldUser) {
+      return res.send({ error: 'User Exists' });
     }
-    catch (error) {
-        res.send({ status: 'error' })
-    }
-})
-
+    await User.create({
+      fname,
+      lname,
+      email,
+      password: encrpytedPassword,
+    });
+    res.send({ status: 'ok' });
+  } catch (error) {
+    res.send({ status: 'error' });
+  }
+});
 
 app.post('/login-user', async (req, res) => {
-    const { email, password } = req.body
-    const user = await User.findOne({ email })
-    if (!user) {
-        return res.json({ error: "User not found" })
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.json({ error: 'User not found' });
+  }
+  if (await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign({ email: user.email }, JWT_SECRET);
+    if (res.status(201)) {
+      return res.json({ status: 'ok', data: token });
+    } else {
+      return res.json({ status: 'error' });
     }
-    if (await bcrypt.compare(password, user.password)) {
-        const token = jwt.sign({ email: user.email }, JWT_SECRET)
-        if (res.status(201)) {
-            return res.json({ status: "ok", data: token })
+  }
 
-        }
-        else {
-            return res.json({ status: "error" })
-        }
-    }
+  res.json({ status: 'error', error: 'Invalid password' });
+});
 
-    res.json({ status: "error", error: "Invalid password" })
-
-})
 
 app.post('/userdata', async (req, res) => {
-    const { token } = req.body
-    try {
-        
-        const user = jwt.verify(token, JWT_SECRET)
-        console.log(user)
-        const useremail = user.email
-        User.findOne({ email: useremail }).then((data) => {
-            res.send({ status: "ok", data: data })
-        }).catch((err) => {
-            res.send({ status: "error", error: err })
-        })
+  const { token } = req.body;
+  try {
+    const user = jwt.verify(token, JWT_SECRET);
+    console.log(user);
+    const useremail = user.email;
+    console.log('useremail', useremail);
+    User.findOne({ email: useremail })
+      .then((data) => {
+        res.send({ status: 'ok', data: data });
+      })
+      .catch((err) => {
+        res.send({ status: 'error', error: err });
+      });
+  } catch (error) {}
+});
 
-    } catch (error) {
-        
-    }
-})
-app.listen(4000, () => console.log("Listening on port 4000"))
+
+app.listen(4000, () => console.log('Listening on port 4000'));
