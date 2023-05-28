b14ec9f7fef25493de0477686cc5388a25367afd
xxshubhamxx <shubham2002garg@gmail.com>
2023-05-28 20:47:06 +0530

    Added Quiz Timer

--- Software-Engineering-Student-Managament-System/src/pages/Quiz.js
+++ Software-Engineering-Student-Managament-System/src/pages/Quiz.js
@@ -4,6 +4,8 @@ import '../Components/css/quiz.css';
 import MainLayout from '../Layout/MainLayout';
 
 function Quiz() {
+  const [timeRemaining, setTimeRemaining] = useState(60); // 1 minute = 60 seconds
+  const [timerActive, setTimerActive] = useState(false);
 
   const [userData, setUserData] = useState('');
   useEffect(() => {
@@ -34,6 +36,32 @@ function Quiz() {
   const [searchParams, setSearchParams] = useSearchParams();
 
   const courseId = searchParams.get("course_id");
+
+   
+  // Timer-related effect
+  useEffect(() => {
+    if (!timerActive || showScore) {
+      return;
+    }
+
+    const timer = setInterval(() => {
+      setTimeRemaining((prevTime) => prevTime - 1);
+    }, 1000);
+
+    if (timeRemaining <= 0) {
+      setShowScore(true);
+      updateMarksOnServer();
+      clearInterval(timer);
+    }
+
+    return () => clearInterval(timer);
+  }, [timerActive, timeRemaining, showScore]);
+
+  useEffect(() => {
+    if (questions.length > 0) {
+      setTimerActive(true);
+    }
+  }, [questions]);
 
   useEffect(() => {
     fetch(`http://localhost:5000/data?course_id=${courseId}`)
@@ -98,47 +126,50 @@ function Quiz() {
   return (
     <MainLayout>
       <div className="quiz">
-        {showScore ? (
-          <div className="score-section">
-            <h2>Quiz Results</h2>
-            <p>You scored {score} out of {questions.length}.</p>
-            <p>
-              {score === questions.length
-                ? "Congratulations! You got all the questions right!"
-                : score >= questions.length / 2
-                ? "Well done! You got more than half the questions right."
-                : "Oops, you didn't get many questions right."}
-            </p>
-            <div className="button-section">
-              <button onClick={handleRestartButtonClick}>Restart Quiz</button>
-              <button onClick={handleExitButtonClick}>Exit Quiz</button>
-            </div>
+        <div className="timer">
+          Time Remaining: {Math.floor(timeRemaining / 60)}:{(timeRemaining % 60).toString().padStart(2, '0')}
           </div>
-        ) : questions.length > 0 ? (
-          <>
-            <div className="question-section">
-              <div className="question-count">
-                <span>Question {currentQuestion + 1}</span>/{questions.length}
-              </div>
-              <div className="question-text">
-                {questions[currentQuestion].question}
-              </div>
-            </div>
-            <div className="answer-section">
-              {questions[currentQuestion].answers.map((answer) => (
-                <button
-                  key={answer.text}
-                  onClick={() => handleAnswerButtonClick(answer.correct)}
-                >
-                  {answer.text}
-                </button>
-              ))}
-            </div>
-          </>
-        ) : (
-          <div>Loading questions...</div>
-        )}
-      </div>
+              {showScore ? (
+                <div className="score-section">
+                  <h2>Quiz Results</h2>
+                  <p>You scored {score} out of {questions.length}.</p>
+                  <p>
+                    {score === questions.length
+                      ? "Congratulations! You got all the questions right!"
+                      : score >= questions.length / 2
+                      ? "Well done! You got more than half the questions right."
+                      : "Oops, you didn't get many questions right."}
+                  </p>
+                  <div className="button-section">
+                    <button onClick={handleRestartButtonClick}>Restart Quiz</button>
+                    <button onClick={handleExitButtonClick}>Exit Quiz</button>
+                  </div>
+                </div>
+              ) : questions.length > 0 ? (
+                <>
+                  <div className="question-section">
+                    <div className="question-count">
+                      <span>Question {currentQuestion + 1}</span>/{questions.length}
+                    </div>
+                    <div className="question-text">
+                      {questions[currentQuestion].question}
+                    </div>
+                  </div>
+                  <div className="answer-section">
+                    {questions[currentQuestion].answers.map((answer) => (
+                      <button
+                        key={answer.text}
+                        onClick={() => handleAnswerButtonClick(answer.correct)}
+                      >
+                        {answer.text}
+                      </button>
+                    ))}
+                  </div>
+                </>
+              ) : (
+            <div>Loading questions...</div>
+          )}
+        </div>
     </MainLayout>
   );
 }
