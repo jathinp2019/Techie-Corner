cbf86237a3f602268b9f1f0d5f3f47ea8f43a2e5
jathinp2019 <jathinp2019@gmail.com>
2023-05-29 09:07:34 +0530

    final fixes

--- Software-Engineering-Student-Managament-System/src/Components/Box.js
+++ Software-Engineering-Student-Managament-System/src/Components/Box.js
@@ -54,35 +54,36 @@ const Box = (props) => {
   const formattedDate = `${year}-${month}-${day}`;
   console.log(formattedDate);
   // console.log(data)
+
   return (
     <MainLayout >
 
       <div className='box1' style={{ background: "transparent" }}>
 
-        <div className='box2' style={{ backgroundColor: 'transparent', boxShadow: "0px 5px 10px 0px rgba(0, 0, 0, 0.5)", color: 'white' }}>
+        <div className='box2' style={{ backgroundColor: 'transparent', boxShadow: "0px 5px 10px 0px rgba(0, 0, 0, 0.5)", color: 'black' }}>
           <span>
             <h1 style={{ color: '#011409', textAlign: 'center',marginLeft:"145px" }}>Scores</h1>
             <h1 style={{ fontSize: 50 }}>
 
-              {/* <span style={{ fontSize: 40, color: 'white', fontWeight: 700 }}> */}
+              {/* <span style={{ fontSize: 40, color: 'black', fontWeight: 700 }}> */}
              
               <p></p>
               <p></p>
               <p />
-              <table className="score">
+              <table className="score" style={{color:"black"}}>
               <tr >
-               <td className='score' style={{padding:"5px"}}><h1>19CSE303</h1> </td> 
-               <td className='score' style={{padding:"5px"}}><h1>{userData.marks_19CSE303}</h1></td>  
+               <td className='score' style={{padding:"10px"}}><h1 style={{color:"black"}}>19CSE303</h1> </td> 
+               <td className='score' style={{padding:"10px"}}><h1 style={{color:"black"}}>{userData.marks_19CSE303}</h1></td>  
                </tr>
 
                <tr>
-               <td className='score' style={{padding:"5px"}}><h1>19CSE304</h1> </td> 
-               <td className='score' style={{padding:"5px"}}><h1>{userData.marks_19CSE304}</h1></td>  
+               <td className='score' style={{padding:"10px"}}><h1 style={{color:"black"}}>19CSE304</h1> </td> 
+               <td className='score' style={{padding:"10px"}}><h1 style={{color:"black"}}>{userData.marks_19CSE304}</h1></td>  
                </tr>
 
                <tr>
-               <td className='score' style={{padding:"5px"}}><h1>19CSE305</h1> </td> 
-               <td className='score' style={{padding:"5px"}}><h1>{userData.marks_19CSE305}</h1></td>  
+               <td className='score' style={{padding:"10px"}}><h1 style={{color:"black"}}>19CSE305</h1> </td> 
+               <td className='score' style={{padding:"10px"}}><h1 style={{color:"black"}}>{userData.marks_19CSE305}</h1></td>  
                </tr>
                
               {/* <span><h1>19CSE304 : {userData.marks_19CSE304}</h1> </span>
@@ -95,32 +96,32 @@ const Box = (props) => {
           </span>
         </div>
 
-        <div className='box3' style={{ backgroundColor: '#transparent', boxShadow: "0px 5px 10px 0px rgba(0, 0, 0, 0.5)", color: 'white' }}>
+        <div className='box3' style={{ backgroundColor: '#transparent', boxShadow: "0px 5px 10px 0px rgba(0, 0, 0, 0.5)", color: 'black' }}>
           <span>
             <h1 style={{ textAlign: 'right', marginLeft: "130px", color: '#011409' }}>Attendance</h1>
             {/* {data} */}
             <CircularProgressBar></CircularProgressBar>
             <br></br>
             <p style={{ marginTop: -27 }}>
-              <span style={{ fontWeight: '500', marginTop: -10}}>{totalattendance}</span>
+              <span style={{ fontWeight: '500', marginTop: -10}}> {totalattendance}</span>
               <span>classes Attended</span><br></br>
               <span style={{ fontWeight: '500' }}>300</span>  classes Conducted
             </p>
           </span>
         </div>
 
-        <div  className='gral' style={{ backgroundColor: '#transparent',boxShadow: "0px 5px 10px 0px rgba(0, 0, 0, 0.5)",color:'white' }}>
+        <div  className='gral' style={{ backgroundColor: '#transparent',boxShadow: "0px 5px 10px 0px rgba(0, 0, 0, 0.5)",color:'black' }}>
         <iframe className="frame" src={userData.graph}></iframe>
 
 
         </div>
-        <div className='lb' style={{ backgroundColor: '#transparent', boxShadow: "0px 5px 10px 0px rgba(0, 0, 0, 0.5)", color: 'white' }}>
+        <div className='lb' style={{ backgroundColor: '#transparent', boxShadow: "0px 5px 10px 0px rgba(0, 0, 0, 0.5)", color: 'black' }}>
           <span>
             <h1 style={{ textAlign: 'center', color: '#011409' }}>Announcements</h1>
             <div className='tab'>
 
                 {annData.map((annData) => (
-                  <table key={annData._id}>
+                  <table key={annData._id} style={{color:"black"}}>
                     <tr>
                       <td>{annData.subject}</td>
                       <td>{annData.type}</td>
